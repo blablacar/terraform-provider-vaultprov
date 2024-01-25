5068872d0c5d7c6cc549f669064cd3b2122c6128
Jeremy Courtial <jeremy.courtial@blablacar.com>
2024-01-25 10:23:57 +0100

    Fix platn modifiers names

--- internal/planmodifiers/int64planmodifiers.go
+++ internal/planmodifiers/int64planmodifiers.go
@@ -8,28 +8,28 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// BoolDefaultValue accepts a types.Bool value and uses the supplied value to set a default
+// Int64DefaultValue accepts a types.Bool value and uses the supplied value to set a default
 // if the config for the attribute is null.
-func BoolDefaultValue(val types.Bool) planmodifier.Bool {
-	return &boolDefaultValueAttributePlanModifier{val}
+func Int64DefaultValue(val types.Int64) planmodifier.Int64 {
+	return &int64DefaultValueAttributePlanModifier{val}
 }
 
-type boolDefaultValueAttributePlanModifier struct {
-	val types.Bool
+type int64DefaultValueAttributePlanModifier struct {
+	val types.Int64
 }
 
-func (d *boolDefaultValueAttributePlanModifier) Description(ctx context.Context) string {
-	return fmt.Sprintf("If not configured, defaults to %t", d.val.ValueBool())
+func (d *int64DefaultValueAttributePlanModifier) Description(ctx context.Context) string {
+	return fmt.Sprintf("If not configured, defaults to %d", d.val.ValueInt64())
 }
 
-func (d *boolDefaultValueAttributePlanModifier) MarkdownDescription(ctx context.Context) string {
+func (d *int64DefaultValueAttributePlanModifier) MarkdownDescription(ctx context.Context) string {
 	return d.Description(ctx)
 }
 
-// PlanModifyBool checks that the value of the attribute in the configuration and assigns the default value if
+// PlanModifyInt64 checks that the value of the attribute in the configuration and assigns the default value if
 // the value in the config is null. This is a destructive operation in that it will overwrite any value
 // present in the plan.
-func (d *boolDefaultValueAttributePlanModifier) PlanModifyBool(ctx context.Context, req planmodifier.BoolRequest, resp *planmodifier.BoolResponse) {
+func (d *int64DefaultValueAttributePlanModifier) PlanModifyInt64(ctx context.Context, req planmodifier.Int64Request, resp *planmodifier.Int64Response) {
 	// Do not set default if the attribute configuration has been set.
 	if !req.ConfigValue.IsNull() {
 		return
