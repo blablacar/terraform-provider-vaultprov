b4382c2bc426ebae8823b8e3f0802e1abc7a4f6d
Jeremy Courtial <jeremy.courtial@blablacar.com>
2026-03-02 11:51:08 +0100

    update dependencies

--- internal/provider/common.go
+++ internal/provider/common.go
@@ -3,6 +3,5 @@ package provider
 const (
 	SecretTypeMetadata   = "secret_type"
 	SecretLengthMetadata = "secret_length"
-	RandomBytesType      = "random_secret"
 	SecretDataKey        = "secret"
 )
